0577b10e636fb7dcf4548da6850db605573e70f8
PiotrLiszka <pliszka@mobiltek>
2019-01-17 08:37:30 +0100

    second commit

--- app/src/main/java/pl/maniak/mygoals/ui/goal/GoalActivity.java
+++ app/src/main/java/pl/maniak/mygoals/ui/goal/GoalActivity.java
@@ -1,26 +1,23 @@
 package pl.maniak.mygoals.ui.goal;
 
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
+import pl.maniak.mygoals.R;
 import pl.maniak.mygoals.model.Goal;
 import pl.maniak.mygoals.ui.BaseActivity;
 import pl.maniak.mygoals.ui.goal.adapters.GoalsAdapter;
-import pl.maniak.mygoals.R;
 
 public class GoalActivity extends BaseActivity {
 
@@ -75,12 +72,8 @@ public class GoalActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
--- app/src/main/java/pl/maniak/mygoals/utils/helpers/DateHelper.java
+++ app/src/main/java/pl/maniak/mygoals/utils/helpers/DateHelper.java
@@ -2,9 +2,10 @@ package pl.maniak.mygoals.utils.helpers;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateHelper {
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
 
     public static String parseDateToString(Date date) {
         return simpleDateFormat.format(date);
